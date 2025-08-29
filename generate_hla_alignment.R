565da7681c4f361a583c45ba20929bf600eb647d
Spandan Pandya <spandan.pandya@gmail.com>
2025-08-29 17:41:18 -0400

    added alighnment generation

--- generate_hla_alignment.R
+++ generate_hla_alignment.R
@@ -15,8 +15,18 @@
 #' @examples
 #' generate_hla_alignment(locus = "A", allele1 = "01:02:01", allele2 = "07:01:01:01", file_path = "pat1_alignment_HLA_C.txt")
 #' 
-#' 
-generate_hla_alignment = function(locus, allele1, allele2, file_path){
+
+
+# Fetch Allele information for testing 
+locus = "C"
+allele1 = "01:02:01"
+allele2 = "07:01:01:01"
+allele1 = paste0(locus,"*",allele1)
+allele2 = paste0(locus,"*",allele2)
+output_path <- "results/Allele1_"
+generate_hla_alighnment(allele1,allele2,locus, filepath = output_path )
+
+#generate_hla_alignment = function(locus, allele1, allele2, file_path){
   
-}
+# }
 
